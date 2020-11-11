de5e12b2d8c0fbb8b5f735ab4bba7d971fc358a3
lailasahb <32404152+lailasahb@users.noreply.github.com>
2020-11-11 19:21:16 -0300

    Add files via upload

---
+++ Forma/src/br/com/lailasahb/git/Forma.java
@@ -0,0 +1,14 @@
+package br.com.lailasahb.git;
+
+public abstract class Forma {
+	
+
+	public String getNome() {
+		return null;
+	}
+
+	public abstract double calcularPerimetro();
+
+	public abstract double calcularArea();
+	
+}
